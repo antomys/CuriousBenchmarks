90003b403f96a63533a85659ad00dbbfc5d489d4
Ihor Volokhovych <ihor.volokhovych@pm.bet>
2022-04-24 11:30:37 +0300

    Reliable benchmarks + revert of Program.cs

--- StringExtensionsBenchmarks/Program.cs
+++ StringExtensionsBenchmarks/Program.cs
@@ -1,5 +1,10 @@
 ﻿using BenchmarkDotNet.Running;
 using StringExtensionsBenchmarks.Benchmarks;
 
+BenchmarkRunner.Run<GenuineStringExtensionsBenchmarks>();
+
 BenchmarkRunner.Run<InterpolationBenchmarks>();
 
+BenchmarkRunner.Run<StackStringExtensionsBenchmarks>();
+
+BenchmarkRunner.Run<StringGenerationBenchmarks>();
