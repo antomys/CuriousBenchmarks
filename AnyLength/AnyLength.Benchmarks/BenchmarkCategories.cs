8024c03e56d3afe260df879bb8cf92f65bd8d6e6
Ihor Volokhovych <igorvolokhovych@gmail.com>
2022-07-29 19:32:05 +0300

    Added Any Length benchmarks (#33)

---
+++ AnyLength/AnyLength.Benchmarks/BenchmarkCategories.cs
@@ -0,0 +1,16 @@
+﻿namespace AnyLength.Benchmarks;
+
+public static class BenchmarkCategories
+{
+    public const string CountExists = "Count Exists";
+    
+    public const string CountPatternExists = "Count Pattern Exists";
+    
+    public const string AnyExists = "Any Exists";
+
+    public const string CountEmpty = "Count Empty";
+    
+    public const string CountPatternEmpty = "Count Pattern Empty";
+    
+    public const string AnyEmpty = "Any Empty";
+}
